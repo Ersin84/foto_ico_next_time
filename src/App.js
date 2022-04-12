3e26b4503aa764ef24ad75d9eedb3ae7ecb6cad4
wromo <info.wromo@gmail.com>
2022-04-12 17:44:27 +0200

    ad

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
-function App() {
+import ImageUpload from './ui/imge-upload/ImageUpload';
+import axios from 'axios';
+ 
+const App = () => {
+  const [files, setFiles] = useState<File[]>([]);
+ 
+  const onDrop = (acceptedFiles: File[]) => {
+    setFiles(acceptedFiles.map(file => Object.assign(file, {
+      preview: URL.createObjectURL(file)
+    })));
+  }
+ 
+  const upload = () => {
+    const uploadURL = 'https://api.cloudinary.com/v1_1/wromo-studio/image/upload/v1647147204/samples/';
+    const uploadPreset = 'ml_140284_wromox';
+ 
+    files.forEach(file => {
+      const formData = new FormData();
+      formData.append('file', file);
+      formData.append('upload_preset', uploadPreset);
+      axios({
+        url: uploadURL,
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        data: formData
+      })
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+    })
+  }
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ImageUpload files={files} onDrop={onDrop}/>
+      <button onClick={() => upload()}>Upload</button>
     </div>
   );
 }
-
+ 
 export default App;
+
